befe90e3535c0e6358b377ef20252c20c482f47e
Snugzz <89753184+Snugzy@users.noreply.github.com>
2021-12-07 13:52:58 -0500

    forge 1.16 branch

---
+++ src/main/java/com/snugz/trulytreasures/Events.java
@@ -0,0 +1,51 @@
+package com.snugz.trulytreasures;
+
+import com.snugz.trulytreasures.config.TrulyTreasuresConfig;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentData;
+import net.minecraft.item.EnchantedBookItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.registry.Registry;
+import net.minecraftforge.common.BasicTrade;
+import net.minecraftforge.event.village.WandererTradesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mod.EventBusSubscriber(modid = TrulyTreasures.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class Events {
+
+    @SubscribeEvent
+    public static void onWandererTrades(WandererTradesEvent event) {
+        List<String> exceptions = TrulyTreasuresConfig.wandererEnchantmentExceptions.get();
+        List<String> cursedEnchantments = TrulyTreasuresConfig.enchantmentsWithCurses.get();
+        List<Enchantment> enchantmentList = TrulyTreasuresConfig.sellCurses.get() ? Registry.ENCHANTMENT.stream().filter((e) -> { return !exceptions.contains(e.getRegistryName().toString()) && e.isTreasureEnchantment() && e.canVillagerTrade(); }).collect(Collectors.toList()) : Registry.ENCHANTMENT.stream().filter((e) -> { return !exceptions.contains(e.getRegistryName().toString()) && e.isTreasureEnchantment() && e.canVillagerTrade() && !e.isCurse(); }).collect(Collectors.toList());
+        List<Enchantment> curseList = Registry.ENCHANTMENT.stream().filter((c) -> { return c.isCurse() && c.canVillagerTrade(); }).collect(Collectors.toList());
+
+        for (Enchantment enchantment : enchantmentList) {
+            for (int level = enchantment.getMinLevel(); level <= enchantment.getMaxLevel(); level++) {
+                ItemStack enchantedBook = EnchantedBookItem.getEnchantedItemStack(new EnchantmentData(enchantment, level));
+                enchantedBook.setCount(1);
+                int basePrice = TrulyTreasuresConfig.basePrice.get();
+                int emeralds = enchantment.getMaxLevel() == 1 ? basePrice * 2 : MathHelper.clamp(basePrice * level, 0, 64);
+                int xp = enchantment.getMaxLevel() == 1 ? 10 : 5 * level;
+
+                if (cursedEnchantments.contains(enchantment.getRegistryName().toString())) {
+                    for (Enchantment curse : curseList) {
+                        for (int curseLevel = curse.getMinLevel(); curseLevel <= curse.getMaxLevel(); curseLevel++) {
+                            ItemStack cursedEnchantedBook = EnchantedBookItem.getEnchantedItemStack(new EnchantmentData(enchantment, level));
+                            cursedEnchantedBook.addEnchantment(curse, curseLevel);
+                            cursedEnchantedBook.setCount(1);
+
+                            event.getRareTrades().add(new BasicTrade(emeralds, cursedEnchantedBook, TrulyTreasuresConfig.maxTrades.get(), xp));
+                        }
+                    }
+                }
+                else event.getRareTrades().add(new BasicTrade(emeralds, enchantedBook, TrulyTreasuresConfig.maxTrades.get(), xp));
+            }
+        }
+    }
+}
---
+++ src/main/java/com/snugz/trulytreasures/mixin/EnchantedBookForEmeraldsTradeMixin.java
@@ -0,0 +1,48 @@
+package com.snugz.trulytreasures.mixin;
+
+import com.snugz.trulytreasures.config.TrulyTreasuresConfig;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentData;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.merchant.villager.VillagerTrades;
+import net.minecraft.item.EnchantedBookItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.MerchantOffer;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.registry.Registry;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Mixin(VillagerTrades.EnchantedBookForEmeraldsTrade.class)
+public class EnchantedBookForEmeraldsTradeMixin {
+    @Shadow
+    @Final
+    private int xpValue;
+
+    @Inject(at = @At("HEAD"), method = "getOffer", cancellable = true)
+    private void removeTreasureEnchantments(Entity trader, Random rand, CallbackInfoReturnable<MerchantOffer> callback) {
+        List<String> exceptions = TrulyTreasuresConfig.villagerEnchantmentExceptions.get();
+        List<Enchantment> list = Registry.ENCHANTMENT.stream().filter((e) -> {
+            return exceptions.contains(e.getRegistryName().toString()) ? e.canVillagerTrade() : !e.isTreasureEnchantment() && e.canVillagerTrade();
+        }).collect(Collectors.toList());
+        Enchantment enchantment = list.get(rand.nextInt(list.size()));
+        int i = MathHelper.nextInt(rand, enchantment.getMinLevel(), enchantment.getMaxLevel());
+        ItemStack itemstack = EnchantedBookItem.getEnchantedItemStack(new EnchantmentData(enchantment, i));
+        int j = 2 + rand.nextInt(5 + i * 10) + 3 * i;
+
+        if (j > 64) {
+            j = 64;
+        }
+
+        callback.setReturnValue(new MerchantOffer(new ItemStack(Items.EMERALD, j), new ItemStack(Items.BOOK), itemstack, 12, this.xpValue, 0.2F));
+    }
+}
